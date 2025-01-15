b1a2adb1035316ee7d3cddc83cb30474e8bcb517
lixoten <lixoten@gmail.com>
2025-01-15 11:14:37 -0800

    Stage 2/5 complete

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -1,6 +1,16 @@
-# Stage 1/5
-bot_name = "Aid"
-birth_year = 2023
+# Stage 2/5
+def greet(bot_name, birth_year):
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
 
-print("Hello! My name is Aid.")
-print("I was created in 2023.")
+
+def remind_name():
+    print("Please, remind me your name.")
+    your_name = input()
+    # reading a name
+    print(f"What a great name you have, {your_name}!")
+
+
+# Now we can use these functions
+greet("Aid", 2023)
+remind_name()
